f5166faa53973b58d291b38d8fbbf94caf85a4c1
thekirankumardash <thekirankumardash@gmail.com>
2016-04-08 12:06:27 +0530

    0506 Customizing the Review Order Section

--- woocommerce/checkout/review-order.php
+++ woocommerce/checkout/review-order.php
@@ -22,36 +22,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 <table class="shop_table woocommerce-checkout-review-order-table">
 	<thead>
 		<tr>
-			<th class="product-name"><?php _e( 'Product', 'woocommerce' ); ?></th>
-			<th class="product-total"><?php _e( 'Total', 'woocommerce' ); ?></th>
+			<th class="product-name">
+				<?php
+					
+					$total_items = WC()->cart->cart_contents_count;
+					if( $total_items == 1 ) {
+						echo $total_items . ' ';
+						_e( 'Product', 'woocommerce' );
+					}else{
+						echo $total_items . ' ';
+						_e( 'Products', 'woocommerce' );						
+					}				
+					
+				?>
+            </th>
+			<th class="product-total"></th>
 		</tr>
 	</thead>
-	<tbody>
-		<?php
-			do_action( 'woocommerce_review_order_before_cart_contents' );
-
-			foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
-				$_product     = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
-
-				if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_checkout_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
-					?>
-					<tr class="<?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
-						<td class="product-name">
-							<?php echo apply_filters( 'woocommerce_cart_item_name', $_product->get_title(), $cart_item, $cart_item_key ) . '&nbsp;'; ?>
-							<?php echo apply_filters( 'woocommerce_checkout_cart_item_quantity', ' <strong class="product-quantity">' . sprintf( '&times; %s', $cart_item['quantity'] ) . '</strong>', $cart_item, $cart_item_key ); ?>
-							<?php echo WC()->cart->get_item_data( $cart_item ); ?>
-						</td>
-						<td class="product-total">
-							<?php echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); ?>
-						</td>
-					</tr>
-					<?php
-				}
-			}
-
-			do_action( 'woocommerce_review_order_after_cart_contents' );
-		?>
-	</tbody>
 	<tfoot>
 
 		<tr class="cart-subtotal">
